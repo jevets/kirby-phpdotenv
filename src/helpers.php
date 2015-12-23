cd785595238a2f003b53f78b204667b7b8f4b447
stevejamesson <steve@evendev.com>
2015-12-23 13:50:36 -0500

    almost there

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,7 @@
 <?php 
 
+use \str as String;
+
 if (! function_exists('env')) {
     /**
      * Gets the value of an environment variable. Supports boolean, empty and null.
@@ -12,8 +14,30 @@ if (! function_exists('env')) {
     {
         $value = getenv($key);
 
-        if ($value === false) {
+        if (!!!$value) {
             return $default;
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+
+            case 'false':
+            case '(false)':
+                return false;
+
+            case 'empty':
+            case '(empty)':
+                return '';
+
+            case 'null':
+            case '(null)':
+                return;
+        }
+
+        if (strlen($value) > 1 && String::startsWith($value, '"') && String::endsWith($value, '"')) {
+            return substr($value, 1, -1);
         }
 
         return $value;
